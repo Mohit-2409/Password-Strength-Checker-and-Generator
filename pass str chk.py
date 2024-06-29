aef7cd0981238df4b0fa846f0e31c14cb5879b26
mc5233 <mohit7052015@gmail.com>
2024-06-30 04:16:41 +0530

    Arrays declared

--- pass str chk.py
+++ pass str chk.py
@@ -2,9 +2,27 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import warnings
-warnings.filterwarnings('ignore')
-data = pd.read_csv("C:/Users/delta/OneDrive/Desktop/VS Code Files/Password Gen and Strength Checker/pass classifier.csv.csv")
-print(data.head())
-print(data[data['password'].isnull()])
-data.dropna(inplace=True)
 
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+warnings.filterwarnings('ignore')
+data = pd.read_csv("C:/Users/delta/OneDrive/Desktop/VS Code Files/Password Gen and Strength Checker/pass classifier.csv")
+#print(data.head())
+#print(data[data['password'].isnull()])
+data.dropna(inplace=True)
+#print(data.isnull().sum())
+#sns.countplot(data['strength'])
+#plt.show()
+
+
+password_tuple = np.array(data)
+print(password_tuple)
+
+import random 
+random.shuffle(password_tuple)
+
+x = [labels[0] for labels in password_tuple]
+y = [labels[1] for labels in password_tuple]
+print(x)
